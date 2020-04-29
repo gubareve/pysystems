b370f8c5f9a7dede87b6c575db41963726d64d39
Evan Gubarev <evan@evangubarev.com>
2020-04-28 19:36:56 -0700

    Update __init__.py

--- pysystems/__init__.py
+++ pysystems/__init__.py
@@ -105,3 +105,8 @@ def solve(*args):
       return "No Unique Value", "No Unique Value", "No Unique Value"
   else:
     return "Only supports systems with 2 or 3 equations"
+
+def test2:
+  assert solve("2x+y=14", "2x+17y=21") == (6.78125, 0.4375)
+def test3:
+  assert solve("2x+7y+3z=5", "3x+12y+z=91", "2x+9y+z=11") == (189.5, -36.5, -39.5)
