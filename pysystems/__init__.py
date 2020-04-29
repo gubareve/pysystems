a465f98f77d533b0af64f95f5a83846e620bd29b
Evan Gubarev <evan@evangubarev.com>
2020-04-28 19:38:11 -0700

    Update __init__.py

--- pysystems/__init__.py
+++ pysystems/__init__.py
@@ -106,7 +106,7 @@ def solve(*args):
   else:
     return "Only supports systems with 2 or 3 equations"
 
-def test2:
+def test2():
   assert solve("2x+y=14", "2x+17y=21") == (6.78125, 0.4375)
-def test3:
+def test3():
   assert solve("2x+7y+3z=5", "3x+12y+z=91", "2x+9y+z=11") == (189.5, -36.5, -39.5)
